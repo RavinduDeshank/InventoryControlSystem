37b71a12c147ac78ada6d6da4f7b6028ef0f2222
Chamudi Weerawickram <chamudisasanka123@gmail.com>
2021-11-19 13:55:44 +0530

    Add project files.

---
+++ InventoryControlSystem/Models/ProductCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace InventoryControlSystem.Models
+{
+    public class ProductCategory
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string CategoryName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string CategoryDiscription { get; set; }
+    }
+}
---
+++ InventoryControlSystem/Models/ProductCategoryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryControlSystem.Models
+{
+    public class ProductCategoryContext : DbContext
+    {
+        public ProductCategoryContext(DbContextOptions<ProductCategoryContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ProductCategory>ProductCategories { get; set; }
+    }
+}
